9dc154240457beaf8994dade86cb766751d5fc1b
liangyy75 <3249475320@qq.com>
2019-07-18 22:54:46 +0800

    Add some tests

--- app/src/main/java/liang/example/loopjtest/MainActivity.java
+++ app/src/main/java/liang/example/loopjtest/MainActivity.java
@@ -1,18 +1,17 @@
-package liang.example.glidetest;
+package liang.example.loopjtest;
 
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+import liang.example.androidtest.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Glide.with(this).load("http://smg.png").fitCenter()
+        setContentView(R.layout.activity_test_handler);
     }
 }
