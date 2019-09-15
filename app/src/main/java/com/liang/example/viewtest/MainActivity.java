509bfcbe6b65e759e6afc5e5c6ffabc9d7badb9c
liangyy75 <3249475320@qq.com>
2019-09-15 20:56:28 +0800

    遗漏提交

---
+++ app/src/main/java/com/liang/example/viewtest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.liang.example.viewtest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.liang.example.androidtest.R;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "View_Main";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        com.liang.example.androidtest.MainActivity.bindActivityList(
+                Constants.names, Constants.descs, Constants.authors, Constants.created, Constants.updated, Constants.classes, this, TAG);
+    }
+}
