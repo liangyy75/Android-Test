ad7f576fa996671b307a27aea09ed973a275d6e5
梁毓颖 <liangyuying.75@bytedance.com>
2020-01-17 15:02:49 +0800

    [update] EasyXmlApi

--- utils-ktx/xml-ktx/src/main/kotlin/com/liang/example/xml_ktx/EasyXmlApi.kt
+++ utils-ktx/xml-ktx/src/main/kotlin/com/liang/example/xml_ktx/EasyXmlApi.kt
@@ -14,7 +14,7 @@ import kotlin.collections.HashMap
 // TODO: xPath
 
 enum class XmlValueType {
-    NUL,
+    // NUL,
     BOOL,
     NUMBER,
     STRING,
@@ -87,7 +87,7 @@ interface EasyXmlNode2 : EasyXmlNode {
         this.strategy = strategy
         children?.forEach {
             if (it is EasyXmlNode2) {
-                it.strategy = strategy
+                it.setRightStrategy(strategy)
             }
             if (it is EasyXmlComment) {
                 it.mStrategy = strategy
@@ -263,9 +263,9 @@ abstract class EasyXmlValueAdapter<T : Any>(var mValue: T?, protected val mType:
     override fun clone(): EasyXmlValue<T> = this
 }
 
-open class EasyXmlNull : EasyXmlValueAdapter<Unit>(null, XmlValueType.NUL) {
-    override fun clone(): EasyXmlValue<Unit> = EasyXmlNull()
-}
+// open class EasyXmlNull : EasyXmlValueAdapter<Unit>(null, XmlValueType.NUL) {
+//     override fun clone(): EasyXmlValue<Unit> = EasyXmlNull()
+// }
 
 open class EasyXmlBool(b: Boolean?) : EasyXmlValueAdapter<Boolean>(b, XmlValueType.BOOL) {
     override fun clone(): EasyXmlValue<Boolean> = EasyXmlBool(mValue)
@@ -479,8 +479,10 @@ open class EasyXmlDeclaration(
     open var mStrategy: XmlStrategy = XmlStrategy.SIMPLEST
     override fun elementType(): XmlNodeType = XmlNodeType.DECLARATION
     override fun string(): String = when (mStrategy) {
-        XmlStrategy.SIMPLEST -> """<?xml version="$version" encoding="$encoding"${attributes?.joinToString("") { " " + it.string() } ?: ""}?>"""
-        else -> "<?xml version=\"$version\" encoding=\"$encoding\"${attributes?.joinToString("") { "\n\t" + it.string() } ?: ""}?>"
+        XmlStrategy.SIMPLEST -> """<?xml version="$version" encoding="$encoding"${attributes?.joinToString("") { " " + it.string() }
+                ?: ""}?>"""
+        else -> "<?xml version=\"$version\" encoding=\"$encoding\"${attributes?.joinToString("") { "\n\t" + it.string() }
+                ?: ""}?>"
     }
 }
 
@@ -492,7 +494,6 @@ open class EasyXmlComment(open var mContent: String, open var mStrategy: XmlStra
 }
 
 const val EMPTY_STRING = ""
-val XML_VALUE_NULL = EasyXmlNull()
 val XML_VALUE_FALSE = EasyXmlBool(false)
 val XML_VALUE_TRUE = EasyXmlBool(true)
 val XML_VALUE_EMPTY_STRING = EasyXmlString(EMPTY_STRING)
@@ -515,6 +516,7 @@ open class EasyXmlParser {
         protected var mType: Int? = START_DOCUMENT
         protected var mText = StringBuilder()
         protected var mTag: String? = null
+        protected var mStartTagClosed: Boolean = false
 
         protected val mTagPath = mutableListOf<String>()
         protected val mAttributes = HashMap<String, String>()
@@ -541,7 +543,8 @@ open class EasyXmlParser {
             mEffectiveTagChars.add('_')
             mEffectiveTagChars.add('.')
 
-            mEffectiveTagChars.addAll(mEffectiveTagChars)
+            mEffectiveAttrChars.addAll(mEffectiveTagChars)
+            mEffectiveAttrChars.add(':')
         }
 
         // < 后面的内容
@@ -594,6 +597,7 @@ open class EasyXmlParser {
                     if (xmlStr[mIndex] == '>') {
                         if (ch == '/') {
                             mTagPath.removeAt(mTagPath.size - 1)
+                            mStartTagClosed = true
                         }
                         mIndex++
                         break
@@ -605,6 +609,9 @@ open class EasyXmlParser {
 
         // </ 后面的内容
         protected fun parseEndTag(): Int? {
+            if (mTagPath.isEmpty()) {
+                return makeFail<Int>("xml format error!!!")
+            }
             val begin = mIndex
             var ch = getNextChar() ?: return null
             while (ch != '>') {
@@ -626,7 +633,7 @@ open class EasyXmlParser {
         }
 
         protected fun parseText(): Int? {
-            if (mType != START_TAG || mType != END_TAG || mType != ENTITY_REF) {
+            if (mType != START_TAG || mType != END_TAG || mType != ENTITY_REF || mType != COMMENT || mTagPath.isEmpty()) {
                 return makeFail<Int>("text only can be placed as element's child: $mType")
             }
             var ch = getNextChar() ?: return null
@@ -676,7 +683,7 @@ open class EasyXmlParser {
             if (mType == START_DOCUMENT) {
                 return makeFail<Int>("empty document!!!")
             }
-            if (mType != END_TAG) {
+            if (mType != END_TAG || mType != COMMENT || mType == END_DOCUMENT || mType == TEXT) {
                 return makeFail<Int>("xml format error!!!")
             }
             mType = END_DOCUMENT
@@ -686,7 +693,8 @@ open class EasyXmlParser {
         open fun getNext(): Int? {
             consumeWhiteSpaces()
             mTag = null
-            if (mIndex >= mLength) {
+            mStartTagClosed = false
+            if (mIndex >= mLength || mType == END_TAG && mTagPath.isEmpty()) {
                 return parseEndDocument()
             }
             var ch = xmlStr[mIndex++]
@@ -785,40 +793,210 @@ open class EasyXmlParser {
             return result
         }
 
-        override fun toString(): String {
-            return "failReason: $mFailReason, index: $mIndex, length: $mLength, less: ${if (mIndex < mLength) xmlStr.substring(mIndex) else "none"}, " +
-                    "type: ($mType: ${EVENT_NAMES[mType]}), text: $mText, tag: $mTag, tagPath: ${mTagPath.joinToString(" --> ")}, " +
-                    "attributes: ${mAttributes.toList().joinToString { "(${it.first} : ${it.second})" }}"
-        }
+        override fun toString(): String = "failReason: $mFailReason, index: $mIndex, length: $mLength, less:\n" +
+                "${if (mIndex < mLength) xmlStr.substring(mIndex) else "none"},\n" +
+                "type: ($mType: ${EVENT_NAMES[mType]}), text: $mText, tag: $mTag, startTagClosed: ${mStartTagClosed},\n" +
+                "tagPath: ${mTagPath.joinToString(" --> ")},\n" +
+                "attributes: ${showMap(mAttributes)},\n" +
+                "entityMap: ${showMap(mEntityMap)}, escapeMap: ${showMap(mEscapeMap)},\n" +
+                "effectiveTagChars: ${mEffectiveTagChars.joinToString()}, " +
+                "effectiveAttrChars: ${mEffectiveAttrChars.joinToString()}"
+
+        protected fun showMap(map: Map<String, String>) = map.toList().joinToString { "(${it.first} : ${it.second})" }
 
         open fun getEventType() = mType
         open fun getFailReason() = mFailReason
         open fun getText() = mText.toString()
         open fun getTagPath() = mTagPath
         open fun getTag() = mTag
+        open fun getStartTagClosed() = mStartTagClosed
         open fun getAttribute(key: String) = mAttributes[key]
         open fun getAttributes() = mAttributes
         open fun attrsIterator() = mAttributes.iterator()
     }
 
     open class EasyXmlSaxParseTask {
-        // TODO
+        open fun run(xmlStr: String, throwEx: Boolean, handler: ContentHandler) {
+            val task = EasyXmlPullParseTask(xmlStr, throwEx)
+            var event = task.getEventType()
+            while (event != END_DOCUMENT) {
+                when (event) {
+                    START_DOCUMENT -> handler.startDocument()
+                    XML_DECL -> handler.xmlDeclaration(task.getAttributes(), task.getFailReason())
+                    START_TAG -> handler.startTag(task.getTag(), task.getAttributes(), task.getStartTagClosed(), task.getFailReason())
+                    TEXT -> handler.text(task.getText(), task.getFailReason())
+                    END_TAG -> handler.endTag(task.getTag(), task.getFailReason())
+                    END_DOCUMENT -> handler.endDocument(task.getFailReason())
+
+                    COMMENT -> handler.comment(task.getText(), task.getFailReason())
+                    CDSECT -> handler.cdata(task.getText(), task.getFailReason())
+                    PROCESSING_INSTRUCTION -> handler.processingInstruction(task.getText(), task.getFailReason())
+                    DOCDECL -> handler.docType(task.getText(), task.getFailReason())
+
+                    else -> handler.unknown(event)
+                }
+                if (task.getNext() == null) {
+                    handler.error(task.getFailReason())
+                    return
+                }
+                event = task.getEventType()
+            }
+            handler.endDocument(task.getFailReason())
+        }
+
+        interface ContentHandler {
+            fun event(eventType: Int?, failReason: String?)
+            fun error(failReason: String?)
+            fun unknown(eventType: Int?) = Unit
+
+            fun startDocument()
+            fun xmlDeclaration(attributes: HashMap<String, String>?, failReason: String?)
+            fun startTag(tagName: String?, attributes: HashMap<String, String>?, tagClosed: Boolean, failReason: String?)
+            fun text(text: String?, failReason: String?)
+            fun endTag(tagName: String?, failReason: String?)
+            fun endDocument(failReason: String?)
+
+            fun comment(text: String?, failReason: String?) = Unit
+            fun cdata(text: String?, failReason: String?) = Unit
+            fun processingInstruction(text: String?, failReason: String?) = Unit
+            fun docType(text: String?, failReason: String?) = Unit
+        }
     }
 
     open class EasyXmlDomParseTask {
         protected var mResult: EasyXmlDocument? = null
         protected var mFailReason: String? = null
 
-        open fun parseXml(xmlStr: String): EasyXmlDocument = parseXmlOrNull(xmlStr)!!
-        open fun parseXmlOrThrow(xmlStr: String): EasyXmlDocument = parseXmlOrNull(xmlStr) ?: throw RuntimeException(mFailReason ?: "unknown reason")
-        open fun parseXmlOrNull(xmlStr: String): EasyXmlDocument? {
-            TODO()
+        open fun parseXml(xmlStr: String): EasyXmlDocument = parseXmlByPull(xmlStr, false)!!
+        open fun parseXmlOrNull(xmlStr: String): EasyXmlDocument? = parseXmlByPull(xmlStr, false)
+        open fun parseXmlOrThrow(xmlStr: String): EasyXmlDocument = parseXmlByPull(xmlStr, true)
+                ?: throw RuntimeException(mFailReason ?: "unknown reason")
+
+        open fun parseXmlByPull(xmlStr: String, throwEx: Boolean): EasyXmlDocument? {
+            val task = EasyXmlPullParseTask(xmlStr, throwEx)
+            var xmlDeclaration: EasyXmlDeclaration? = null
+            var event = task.getEventType()
+            var first = true
+            var currentParent: EasyXmlNode2? = null
+            while (event != END_DOCUMENT) {
+                when (event) {
+                    XML_DECL -> xmlDeclaration = parseXmlDeclaration(task) ?: return null
+                    START_TAG -> {
+                        if (first) {
+                            xmlDeclaration = xmlDeclaration ?: EasyXmlDeclaration()
+                            mResult = EasyXmlDocument(task.getTag()
+                                    ?: return makeFail<EasyXmlDocument>("parse error: document tag is null!!!", task.throwEx), xmlDeclaration)
+                            task.getAttributes().forEach {
+                                mResult!!.addAttr(parseAttribute(it.toPair(), task.throwEx) ?: return null)
+                            }
+                            currentParent = mResult
+                            first = false
+                        } else {
+                            val element = EasyXmlElement(task.getTag()
+                                    ?: return makeFail<EasyXmlDocument>("parse error: start tag is null!!!", task.throwEx))
+                            task.getAttributes().forEach { element.addAttr(parseAttribute(it.toPair(), task.throwEx) ?: return null) }
+                            currentParent!!.add(element)
+                            if (!task.getStartTagClosed()) {
+                                currentParent = element
+                            }
+                        }
+                    }
+                    END_TAG -> {
+                        if (task.getTag() != currentParent!!.tag()) {
+                            val failReason = "parse error: end tag(${task.getTag()}), but start tag(${currentParent.tag()})"
+                            return makeFail<EasyXmlDocument>(failReason, task.throwEx)
+                        }
+                        currentParent = currentParent.parent
+                    }
+                    TEXT -> currentParent!!.add(EasyXmlText(task.getText()))
+                    else -> Unit /* 忽略 cdata / comment / doctype / processing instruction 等等 */
+                }
+                if (task.getNext() == null) {
+                    mFailReason = task.getFailReason()
+                    return null
+                }
+                event = task.getEventType()
+            }
+            return mResult
+        }
+
+        protected fun parseXmlDeclaration(task: EasyXmlPullParseTask): EasyXmlDeclaration? {
+            val attributes = task.getAttributes()
+            val version = attributes["version"] ?: DEFAULT_VERSION
+            val encoding = attributes["encoding"] ?: DEFAULT_ENCODING
+            attributes.remove("version")
+            attributes.remove("encoding")
+            val xmlAttributes: MutableList<EasyXmlAttribute>? = if (attributes.isEmpty()) {
+                null
+            } else {
+                val temp = mutableListOf<EasyXmlAttribute>()
+                for (attr in attributes) {
+                    temp.add(parseAttribute(attr.toPair(), task.throwEx) ?: return null)
+                }
+                temp
+            }
+            return EasyXmlDeclaration(version, encoding, xmlAttributes)
+        }
+
+        protected fun parseAttribute(nameValue: Pair<String, String>, throwEx: Boolean): EasyXmlAttribute? {
+            val name = nameValue.first
+            // val value = nameValue.second
+            // val xmlValue = if (value.isNotEmpty()) {
+            //     when {
+            //         value == "true" -> XML_VALUE_TRUE
+            //         value == "false" -> XML_VALUE_FALSE
+            //         value[0] == '-' || value[0] in allDigitCharRange -> {
+            //             var decimalFlag = false
+            //             for (ch in value) {
+            //                 if (ch == '.' || ch == 'e' || ch == 'E') {
+            //                     decimalFlag = true
+            //                     break
+            //                 }
+            //             }
+            //             try {
+            //                 if (decimalFlag) {
+            //                     val d = value.toDouble()
+            //                     if (!d.isInfinite()) {
+            //                         EasyXmlNumber(d)
+            //                     } else {
+            //                         EasyXmlNumber(value.toBigDecimal())
+            //                     }
+            //                 } else {
+            //                     try {
+            //                         EasyXmlNumber(value.toLong())
+            //                     } catch (e: NumberFormatException) {
+            //                         EasyXmlNumber(value.toBigInteger())
+            //                     }
+            //                 }
+            //             } catch (e: NumberFormatException) {
+            //                 EasyXmlString(value)
+            //             }
+            //         }
+            //         else -> EasyXmlString(value)
+            //     }
+            // } else {
+            //     EasyXmlString("")
+            // }
+            val xmlValue = EasyXmlString(nameValue.second)
+            return when (val splitIndex = name.indexOf(':')) {
+                -1 -> EasyXmlAttribute(name, xmlValue)
+                else -> EasyXmlAttribute(name.substring(splitIndex + 1), xmlValue, name.substring(0, splitIndex))
+            }
+        }
+
+        protected fun <T> makeFail(reason: String, throwEx: Boolean, result: T? = null): T? {
+            mFailReason = reason
+            if (throwEx) {
+                throw RuntimeException(mFailReason)
+            }
+            return result
         }
     }
 
     companion object {
         @SuppressLint("UseSparseArrays")
         val EVENT_NAMES = HashMap<Int, String>()
+        val allDigitCharRange = '0'..'9'
 
         /** pull */
         const val START_DOCUMENT = 0
